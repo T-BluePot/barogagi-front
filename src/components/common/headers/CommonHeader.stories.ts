a4cba58cd5108c4aff6e967f5c5ec61ef3576733
Leeseoleem <leeseorim0029@gmail.com>
2025-06-19 00:56:15 +0900

    feat: Common Header 스토리북 연동

---
+++ src/components/common/headers/CommonHeader.stories.ts
@@ -0,0 +1,18 @@
+import type { Meta, StoryObj } from "@storybook/react-vite";
+import { action } from "storybook/actions";
+
+import { CommonHeader } from "./CommonHeader";
+
+const meta = {
+  title: "Example/CommonHeader",
+  component: CommonHeader,
+  parameters: {
+    layout: "fullscreen",
+  },
+  args: { onClick: action("clicked") },
+} satisfies Meta<typeof CommonHeader>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {};
