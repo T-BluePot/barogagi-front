c3b16fb03f30186a863dc5f13aedda153d5b2195
Leeseoleem <leeseorim0029@gmail.com>
2025-06-18 18:55:47 +0900

    chore: 테스트 용 코드 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
 import "./App.css";
+import { useState } from "react";
+import { CommonTag } from "./components/common/tags/commonTag";
+import { SelectTag } from "./components/common/tags/SelectTag";
 
 function App() {
+  const [active, setActive] = useState<boolean>(true);
   return (
     <div className="min-h-screen flex items-center justify-center">
-      <div className="text-title-01 text-main-dark bg-main-disable font-bold p-4 rounded-card">
+      <div className="text-subtitle text-gray-black bg-main-disable font-bold p-4 rounded-card">
         테스트
       </div>
+      <CommonTag
+        size="small"
+        label="테스트"
+        active={active}
+        onClick={() => setActive(!active)}
+      />
+      <SelectTag label="테스트2" onClick={() => setActive(!active)} />
+      <p className="text-header leading-header tracking-header">제목</p>
     </div>
   );
 }
