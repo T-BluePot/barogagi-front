d9380fcf8b1ada5ea35b2049ece7172643d31939
Leeseoleem <leeseorim0029@gmail.com>
2025-06-19 00:34:00 +0900

    feat: Title Header 컴포넌트 구현

---
+++ src/components/common/headers/TitleHeader.tsx
@@ -0,0 +1,15 @@
+import clsx from "clsx";
+
+interface TitleHeaderProps {
+  label: string;
+  isDarkBg?: boolean;
+}
+
+export const TitleHeader = ({ isDarkBg = false, label }: TitleHeaderProps) => {
+  const colorClass = clsx(isDarkBg ? "text-gray-white" : "text-gray-black");
+  return (
+    <header className="flex w-screen h-header justify-center items-center px-screen gap-lg">
+      <span className={clsx("typo-title-01", colorClass)}>{label}</span>
+    </header>
+  );
+};
