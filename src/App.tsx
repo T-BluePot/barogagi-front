e1eba374157e9e1823f33c1cac532661dade05c9
Leeseoleem <leeseorim0029@gmail.com>
2025-06-18 15:17:30 +0900

    chore: App 파일 초기화

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,8 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div className="min-h-screen flex items-center justify-center">
-      <h1 className="text-4xl font-bold text-main-500">
-        Hello, Tailwind CSS 4.0!
-      </h1>
       <div className="text-title-01 text-main-dark bg-main-disable font-bold p-4 rounded-card">
         테스트
       </div>
