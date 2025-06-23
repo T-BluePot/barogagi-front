64aa87c6942bce4d590aec18666287ceaf998600
정은우 <136785138+Jung-eunwoo@users.noreply.github.com>
2025-06-23 22:38:32 +0900

    design: 접근성을 위해 키보드 포커싱 시 outline 추가

--- src/components/common/buttons/TextButton.tsx
+++ src/components/common/buttons/TextButton.tsx
@@ -3,7 +3,7 @@ type Props = {
 };
 
 const TextButton = ({ label }: Props) => {
-  const baseStyle = `px-4 py-3 typo-body cursor-pointer transition-colors duration-200 focus:outline-none `;
+  const baseStyle = `px-4 py-3 typo-body cursor-pointer transition-colors duration-200 focus:ring-2 focus:ring-blue-500 focus:ring-offset-2`;
   const typoStyle = `text-gray-50 hover:text-black break-words`;
 
   return (
