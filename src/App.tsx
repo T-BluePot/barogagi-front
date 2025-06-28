7e4bd7d582f645dc7aa4a1028aa09a5f60a9d9c4
정은우 <136785138+Jung-eunwoo@users.noreply.github.com>
2025-06-28 21:24:26 +0900

    refactor: 라우팅 구조 개선 - BrowserRouter를 App 최상위로 이동

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import "./App.css";
 import DevNav from "@/components/DevNav";
 import { AuthRoutes } from "@/routes/AuthRoutes";
+import { BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <DevNav />
       <AuthRoutes />
-    </>
+    </BrowserRouter>
   );
 }
 
--- src/routes/AuthRoutes.tsx
+++ src/routes/AuthRoutes.tsx
@@ -1,10 +1,8 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import AuthLandingPage from "@/pages/auth/AuthLandingPage";
 
 export const AuthRoutes = () => (
-  <BrowserRouter>
-    <Routes>
-      <Route path="/login" element={<AuthLandingPage />} />
-    </Routes>
-  </BrowserRouter>
+  <Routes>
+    <Route path="/login" element={<AuthLandingPage />} />
+  </Routes>
 );
