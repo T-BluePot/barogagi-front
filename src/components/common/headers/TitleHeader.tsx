bfd0254d283e72c1d07ab0f402b2ce2638eab422
Leeseoleem <leeseorim0029@gmail.com>
2025-07-04 16:53:18 +0900

    refactor: title header 컴포넌트에 children 타입 추가
    - children 타입을 통해 title header 컴포넌트에 jsx 요소 삽입 가능

--- src/components/common/headers/TitleHeader.tsx
+++ src/components/common/headers/TitleHeader.tsx
@@ -1,15 +1,28 @@
 import clsx from "clsx";
+import type { ReactNode } from "react";
 
 interface TitleHeaderProps {
   label: string;
   isDarkBg?: boolean;
+  children?: ReactNode;
 }
 
-export const TitleHeader = ({ isDarkBg = false, label }: TitleHeaderProps) => {
-  const colorClass = clsx(isDarkBg ? "text-gray-white" : "text-gray-black");
+export const TitleHeader = ({
+  isDarkBg = false,
+  label,
+  children,
+}: TitleHeaderProps) => {
   return (
-    <header className="flex px-6 w-screen h-[60px] items-center gap-6 select-none">
-      <span className={clsx("typo-title-02", colorClass)}>{label}</span>
+    <header className="flex px-6 w-full h-[60px] justify-between items-center gap-6 select-none bg-alert-red">
+      <span
+        className={clsx(
+          "typo-title-02",
+          isDarkBg ? "text-gray-white" : "text-gray-black"
+        )}
+      >
+        {label}
+      </span>
+      {children && <div className="ml-auto flex items-center">{children}</div>}
     </header>
   );
 };
