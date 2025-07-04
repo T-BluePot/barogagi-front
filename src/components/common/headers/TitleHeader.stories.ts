a0efdd54a97244711fff9cef4749728825878aa5
Leeseoleem <leeseorim0029@gmail.com>
2025-07-04 17:09:17 +0900

    refactor: title header 컴포넌트 스토리북 추가

--- src/components/common/headers/TitleHeader.stories.ts
+++ src/components/common/headers/TitleHeader.stories.ts
@@ -5,10 +5,16 @@ import { TitleHeader } from "./TitleHeader";
 const meta = {
   title: "Components/TitleHeader",
   component: TitleHeader,
+  tags: ["autodocs"],
   parameters: {
     layout: "fullscreen",
   },
   argTypes: {
+    label: {
+      control: "text",
+      description: "헤더 좌측 텍스트",
+      defaultValue: "기본 텍스트 헤더",
+    },
     isDarkBg: {
       control: "boolean",
       description: "배경이 어두운지 여부",
@@ -23,5 +29,14 @@ type Story = StoryObj<typeof meta>;
 export const Default: Story = {
   args: {
     label: "기본 텍스트 헤더",
+    isDarkBg: false,
+  },
+};
+
+export const WithChildren: Story = {
+  args: {
+    label: "추가 텍스트 헤더",
+    isDarkBg: false,
+    children: "해당 위치에 JSX 요소 추기",
   },
 };
