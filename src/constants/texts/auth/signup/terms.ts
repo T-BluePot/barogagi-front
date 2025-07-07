2ac1edd0eecf6c780841453fd79d4ad112faee8f
Leeseoleem <leeseorim0029@gmail.com>
2025-07-08 03:59:36 +0900

    chore: 이용 약관 동의 페이지 텍스트 분리

---
+++ src/constants/texts/auth/signup/terms.ts
@@ -0,0 +1,10 @@
+export const TERMS_TEXT = {
+  title: "바로가기 서비스를 이용하기 위해\n약관에 동의해주세요",
+  agreeAll: "전체 동의하기",
+
+  termsOfServiceRequired: "이용약관 동의 (필수)",
+  privacyPolicyRequired: "개인정보 수집 및 이용 동의 (필수)",
+  marketingOptional: "마케팅 정보 수신 동의 (선택)",
+
+  nextButton: "다음",
+} as const;
