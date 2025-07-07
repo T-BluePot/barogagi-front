8097cfe084a47b6c090a1e870c55cbe473776782
Leeseoleem <leeseorim0029@gmail.com>
2025-07-07 19:07:34 +0900

    style: 버튼 요소에 cursor-pointer 추가

--- src/components/common/bottom-modal/BottomModalHeader.tsx
+++ src/components/common/bottom-modal/BottomModalHeader.tsx
@@ -9,13 +9,19 @@ export const BottomModalHeader = ({
   return (
     <div className="flex h-16 items-center justify-between px-6">
       {variant !== "title" && (
-        <button onClick={onCancel} className="typo-subtitle text-gray-60">
+        <button
+          onClick={onCancel}
+          className="typo-subtitle text-gray-60 cursor-pointer"
+        >
           취소
         </button>
       )}
       <span className="typo-title-02 text-gray-black">{title}</span>
       {variant !== "title" && (
-        <button onClick={onConfirm} className="typo-subtitle text-gray-black">
+        <button
+          onClick={onConfirm}
+          className="typo-subtitle text-gray-black cursor-pointer"
+        >
           확인
         </button>
       )}
