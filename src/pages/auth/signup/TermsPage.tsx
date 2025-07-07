954dcf0363e77b32855d4c14c2736213a3072017
Leeseoleem <leeseorim0029@gmail.com>
2025-07-08 03:52:35 +0900

    chore: 테스트 용 페이지 업데이트

--- src/pages/auth/signup/TermsPage.tsx
+++ src/pages/auth/signup/TermsPage.tsx
@@ -1,15 +1,67 @@
+import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import { TitleHeader } from "@/components/common/headers/TitleHeader";
+import { TERMS_TEXT } from "@/constants/texts/auth/signup/terms";
 
-type Props = {};
+import { BackHeader } from "@/components/common/headers/BackHeader";
+import { TermsAgreementItem } from "./components/TermsAgreementItem";
+import Button from "@/components/common/buttons/CommonButton";
 
-const TermsPage = (props: Props) => {
+import { CheckBoxButton } from "@/components/auth/common/CheckBoxButton";
+
+const TermsPage = () => {
   const navigate = useNavigate();
 
+  const [isAgreeAll, setIsAgreeAll] = useState(false);
+  const [isAgreePrivacy, setIsAgreePrivacy] = useState(false);
+  const [isAgreeMarketing, setIsAgreeMarketing] = useState(false);
+
+  useEffect(() => {
+    setIsAgreeAll(isAgreePrivacy && isAgreeMarketing);
+  }, [isAgreePrivacy, isAgreeMarketing]);
+
+  const handleAgreeAll = () => {
+    setIsAgreeAll(!isAgreeAll);
+    setIsAgreePrivacy(!isAgreePrivacy);
+    setIsAgreeMarketing(!isAgreeMarketing);
+  };
+
   return (
-    <div className="w-screen bg-amber-100">
-      <TitleHeader label="네" />
-      <span>gkdl</span>
+    <div className="flex flex-col w-full min-h-screen bg-gray-black">
+      <BackHeader isDarkBg={true} onClick={() => console.log("뒤로 가기")} />
+      <div className="flex flex-col flex-1 w-full px-6 items-baseline">
+        <span className="typo-title-01 my-[60px] text-white text-left whitespace-pre-line">
+          {TERMS_TEXT.title}
+        </span>
+        <div className="flex flex-col w-full gap-4">
+          <CheckBoxButton
+            label={TERMS_TEXT.agreeAll}
+            size="large"
+            isChecked={isAgreeAll}
+            setIsChecked={handleAgreeAll}
+          />
+
+          <div>
+            <TermsAgreementItem
+              label={TERMS_TEXT.privacyPolicyRequired}
+              isSelected={isAgreePrivacy}
+              toggleSelected={() => setIsAgreePrivacy(!isAgreePrivacy)}
+              onClick={() => console.log("필수")}
+            />
+            <TermsAgreementItem
+              label={TERMS_TEXT.marketingOptional}
+              isSelected={isAgreeMarketing}
+              toggleSelected={() => setIsAgreeMarketing(!isAgreeMarketing)}
+              onClick={() => console.log("선택")}
+            />
+          </div>
+        </div>
+      </div>
+      <div className="mt-auto w-full p-6">
+        <Button
+          label={TERMS_TEXT.nextButton}
+          onClick={() => navigate("/signup/credentials")}
+        />
+      </div>
     </div>
   );
 };
