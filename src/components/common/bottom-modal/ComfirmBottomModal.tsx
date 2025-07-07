33fad03379c324dd665a7d97e2a600ef7d7d5c12
Leeseoleem <leeseorim0029@gmail.com>
2025-07-07 19:08:44 +0900

    feat: ConfirmBottomModal 컴포넌트 추가

---
+++ src/components/common/bottom-modal/ComfirmBottomModal.tsx
@@ -0,0 +1,20 @@
+import type { ConfirmBottomModalProps } from "@/types/BottomModalTypes";
+
+import { BottomModalLayout } from "@/components/layout/BottomModalLayout";
+import { BottomModalHeader } from "./BottomModalHeader";
+import { BottomModalDeleteButton } from "./BottomModalDeleteButton";
+
+export const ConfirmBottomModal = ({
+  children,
+  withDelete, // 삭제 버튼 유무
+  onDelete,
+  ...layoutProps
+}: ConfirmBottomModalProps) => {
+  return (
+    <BottomModalLayout {...layoutProps}>
+      <BottomModalHeader variant="detail" title={layoutProps.title} />
+      {children}
+      {withDelete && <BottomModalDeleteButton onClick={onDelete} />}
+    </BottomModalLayout>
+  );
+};
