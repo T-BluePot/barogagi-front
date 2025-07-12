dae6a6b9757462a3bbd70e7a69c673c2efb88c41
Leeseoleem <leeseorim0029@gmail.com>
2025-07-12 18:45:53 +0900

    feat: CREDENTIALS 화면용 텍스트 상수 생성

---
+++ src/constants/texts/auth/signup/credentials.ts
@@ -0,0 +1,19 @@
+export const CREDENTIALS_TEXT = {
+  TITLE: "로그인에 필요한 정보를\n입력해주세요",
+  NEXT_BUTTON: "다음",
+};
+
+export const ID_MESSAGES = {
+  REQUIRED: "아이디를 입력해주세요.",
+  INVALID_FORMAT: "아이디는 영문 소문자 시작, 숫자 포함 4~16자여야 합니다.",
+  NO_WHITESPACE: "공백은 포함할 수 없습니다.",
+  NO_SPECIAL_CHAR: "특수문자는 사용할 수 없습니다. (_ - @ 등)",
+  MUST_START_WITH_LETTER: "아이디는 영문 소문자로 시작해야 합니다.",
+};
+
+export const PASSWORD_MESSAGES = {
+  REQUIRED: "비밀번호를 입력해주세요.",
+  INVALID_FORMAT: "비밀번호는 영문과 숫자를 포함한 8~20자여야 합니다.",
+  NO_WHITESPACE: "공백은 포함할 수 없습니다.",
+  ALLOWED_SPECIALS: "사용할 수 없는 특수 문자가 포함되어 있습니다.",
+};
