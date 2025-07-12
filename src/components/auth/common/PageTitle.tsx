88faf67e8d3aad190d8c4b73425dfc586173f556
Leeseoleem <leeseorim0029@gmail.com>
2025-07-13 03:43:28 +0900

    feat: 서브 타이틀 포함 Page Title 컴포넌트 생성

--- src/components/auth/common/PageTitle.tsx
+++ src/components/auth/common/PageTitle.tsx
@@ -5,3 +5,22 @@ export const PageTitle = ({ title }: { title: string }) => {
     </span>
   );
 };
+
+export const PageTitleWithSub = ({
+  title,
+  subTitle,
+}: {
+  title: string;
+  subTitle: string;
+}) => {
+  return (
+    <div className="flex flex-col w-full gap-3 my-[60px]">
+      <span className="typo-title-01 text-white text-left whitespace-pre-line">
+        {title}
+      </span>
+      <span className="typo-body text-gray-20 text-left whitespace-pre-line">
+        {subTitle}
+      </span>
+    </div>
+  );
+};
