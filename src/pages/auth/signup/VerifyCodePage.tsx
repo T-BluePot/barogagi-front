cd4dd6c00111511b7853c6824ef1af4447956de3
Leeseoleem <leeseorim0029@gmail.com>
2025-07-13 04:29:32 +0900

    feat: VerifyCodePage 1차 구현 완료

--- src/pages/auth/signup/VerifyCodePage.tsx
+++ src/pages/auth/signup/VerifyCodePage.tsx
@@ -1,5 +1,5 @@
 import { useNavigate } from "react-router-dom";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import { safeBack } from "@/utils/safeBack";
 import { VERIFY_TEXT } from "@/constants/texts/auth/signup/verify";
@@ -12,6 +12,34 @@ import Button from "@/components/common/buttons/CommonButton";
 const VerifyCodePage = () => {
   const navigate = useNavigate();
   const [code, setCode] = useState<string>("");
+
+  const [remainingSeconds, setRemainingSeconds] = useState(180); // 3분 = 180초
+  const [formattedTime, setFormattedTime] = useState("03:00");
+
+  // 1초마다 남은 시간 감소
+  useEffect(() => {
+    if (remainingSeconds <= 0) {
+      // 시간 초과 시 실행
+      navigate("/signup/verify");
+      return;
+    }
+
+    const timer = setInterval(() => {
+      setRemainingSeconds((prev) => prev - 1);
+    }, 1000);
+
+    return () => clearInterval(timer); // 컴포넌트 사라질 때 정리
+  }, [remainingSeconds, navigate]);
+
+  // 남은 초를 MM:SS 형식으로 변환
+  useEffect(() => {
+    const minutes = Math.floor(remainingSeconds / 60)
+      .toString()
+      .padStart(2, "0");
+    const seconds = (remainingSeconds % 60).toString().padStart(2, "0");
+    setFormattedTime(`${minutes}:${seconds}`);
+  }, [remainingSeconds]);
+
   return (
     <div className="flex flex-col w-full min-h-screen bg-gray-black">
       <BackHeader
@@ -27,9 +55,11 @@ const VerifyCodePage = () => {
             value={code}
             setValue={setCode}
             type="tel"
+            error={!!formattedTime}
+            helperText={formattedTime}
           />
           <Button
-            label={VERIFY_TEXT.PHONE.NEXT_BUTTON}
+            label={VERIFY_TEXT.CODE.NEXT_BUTTON}
             isDisabled={!code}
             onClick={() => navigate("/signup/verify")}
           />
