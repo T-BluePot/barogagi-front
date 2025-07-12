c1913a50245aa56fb3a58a638eafbbbe6c08d80c
Leeseoleem <leeseorim0029@gmail.com>
2025-07-12 18:35:46 +0900

    feat: CredentialsPage 추가 및 라우트 연동

--- src/routes/AuthRoutes.tsx
+++ src/routes/AuthRoutes.tsx
@@ -1,10 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import AuthLandingPage from "@/pages/auth/AuthLandingPage";
+
+/* auth/signup 로직 */
 import TermsPage from "@/pages/auth/signup/TermsPage";
+import CredentialsPage from "@/pages/auth/signup/CredentialsPage";
 
 export const AuthRoutes = () => (
   <Routes>
     <Route path="/login" element={<AuthLandingPage />} />
     <Route path="/signup" element={<TermsPage />} />
+    <Route path="/signup/credentials" element={<CredentialsPage />} />
   </Routes>
 );
