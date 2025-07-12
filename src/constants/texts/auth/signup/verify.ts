79805b6d4a0b9dc00b4b7a333cf359ca1b65bf72
Leeseoleem <leeseorim0029@gmail.com>
2025-07-13 04:19:08 +0900

    feat: VerifyCodePage 페이;지 전용 텍스트 분리

--- src/constants/texts/auth/signup/verify.ts
+++ src/constants/texts/auth/signup/verify.ts
@@ -1,8 +1,17 @@
 export const VERIFY_TEXT = {
-  TITLE: "휴대폰 본인 인증",
-  SUB_TITLE: "원활한 서비스 이용을 위해 인증이 필요해요",
-  NEXT_BUTTON: "본인 인증하기",
+  PHONE: {
+    TITLE: "휴대폰 본인 인증",
+    SUB_TITLE: "원활한 서비스 이용을 위해 인증이 필요해요",
+    NEXT_BUTTON: "본인 인증하기",
 
-  LABEL: "휴대폰 번호",
-  PLACEHOLDER: "하이픈(-) 없이 숫자만 입력해주세요.",
+    LABEL: "휴대폰 번호",
+    PLACEHOLDER: "하이픈(-) 없이 숫자만 입력해주세요.",
+  },
+  CODE: {
+    TITLE: "휴대폰로 전송된 인증 번호를\n입력해주세요",
+    NEXT_BUTTON: "확인",
+
+    LABEL: "인증 번호",
+    PLACEHOLDER: "인증 번호를 입력해주세요.",
+  },
 };
