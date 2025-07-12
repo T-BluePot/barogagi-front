6ce7118d02c419ffc8e8f7739be0053735abef96
Leeseoleem <leeseorim0029@gmail.com>
2025-07-13 04:18:33 +0900

    feat: VerifyPhonePage 1차 구현 완료

--- src/pages/auth/signup/VerifyPhonePage.tsx
+++ src/pages/auth/signup/VerifyPhonePage.tsx
@@ -1,21 +1,44 @@
 import { useNavigate } from "react-router-dom";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 
 import { safeBack } from "@/utils/safeBack";
+import { VERIFY_TEXT } from "@/constants/texts/auth/signup/verify";
 
 import { BackHeader } from "@/components/common/headers/BackHeader";
-import { PageTitle } from "@/components/auth/common/PageTitle";
+import { PageTitleWithSub } from "@/components/auth/common/PageTitle";
+import { CommonInput } from "@/components/auth/common/CommonInput";
+import Button from "@/components/common/buttons/CommonButton";
 
 const VerifyPhonePage = () => {
   const navigate = useNavigate();
+
+  const [phone, setPhone] = useState<string>("");
+
   return (
     <div className="flex flex-col w-full min-h-screen bg-gray-black">
       <BackHeader
         isDarkBg={true}
-        onClick={() => safeBack(navigate, "/signup")}
+        onClick={() => safeBack(navigate, "/signup/credentials")}
       />
       <div className="flex flex-col w-full px-6">
-        <PageTitle title="하이" />
+        <PageTitleWithSub
+          title={VERIFY_TEXT.PHONE.TITLE}
+          subTitle={VERIFY_TEXT.PHONE.SUB_TITLE}
+        />
+        <div className="flex flex-col gap-6">
+          <CommonInput
+            label={VERIFY_TEXT.PHONE.LABEL}
+            placeholder={VERIFY_TEXT.PHONE.PLACEHOLDER}
+            value={phone}
+            setValue={setPhone}
+            type="tel"
+          />
+          <Button
+            label={VERIFY_TEXT.PHONE.NEXT_BUTTON}
+            isDisabled={!phone}
+            onClick={() => navigate("/signup/verify/code")}
+          />
+        </div>
       </div>
     </div>
   );
