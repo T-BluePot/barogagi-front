d91aec241c452ab23d08d2ac929fdb2dad10a593
Leeseoleem <leeseorim0029@gmail.com>
2025-07-14 01:00:20 +0900

    chore: 프로필 화면 텍스트 추가

--- src/constants/texts/auth/signup/profile.ts
+++ src/constants/texts/auth/signup/profile.ts
@@ -11,6 +11,8 @@ export const PROFILE_TEXT = {
   },
   CHECK_LABEL: "다음에 입력할게요",
   SKIP_MODAL: {
+    CANCLE_LABEL: "취소",
+    CONFIRM_LABEL: "확인",
     TITLE: "프로필 등록을 건너뛰시겠어요?",
     SUB_TITLE: "프로필을 등록하지 않으면\n닉네임이 자동으로 설정됩니다.",
   },
