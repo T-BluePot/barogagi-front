004f1edde62063d77c47015729d0138d835571eb
Leeseoleem <leeseorim0029@gmail.com>
2025-07-14 01:39:56 +0900

    refactor: 뒤로가기 방지 함수 로직 수정
    - 함수 사용 시 뒤로가기 이벤트 발생시에 원하는 라우트로 이동할 수 있게 함

--- src/utils/useBlockBackNavigation.ts
+++ src/utils/useBlockBackNavigation.ts
@@ -1,20 +1,15 @@
 import { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
 
 /**
  * 뒤로가기 방지 훅
  * @param onBack Optional - 뒤로가기 시 실행할 콜백 함수 (예: 상태 감소, 특정 페이지 이동 등)
  */
 export const useBlockBackNavigation = (onBack?: () => void) => {
-  const navigate = useNavigate();
-
   useEffect(() => {
     // 현재 URL을 히스토리에 추가 → 뒤로가기를 누르면 popstate 발생하게 함
     history.pushState(null, "", window.location.href);
 
-    const handlePopState = (e: PopStateEvent) => {
-      e.preventDefault(); // 기본 뒤로가기 동작 막기
-
+    const handlePopState = () => {
       if (onBack) {
         onBack(); // 사용자 정의 동작 실행
       } else {
@@ -27,5 +22,5 @@ export const useBlockBackNavigation = (onBack?: () => void) => {
     return () => {
       window.removeEventListener("popstate", handlePopState);
     };
-  }, [onBack, navigate]);
+  }, [onBack]);
 };
