9b9f8658e0bc0baa92e1b481dfc3da9cbdbc3470
Leeseoleem <leeseorim0029@gmail.com>
2025-07-14 17:59:12 +0900

    refactor: 페이지 타이틀 컴포넌트 병합

--- src/components/auth/common/PageTitle.tsx
+++ src/components/auth/common/PageTitle.tsx
@@ -1,26 +1,20 @@
-export const PageTitle = ({ title }: { title: string }) => {
-  return (
-    <span className="typo-title-01 my-[60px] text-white text-left whitespace-pre-line">
-      {title}
-    </span>
-  );
-};
-
-export const PageTitleWithSub = ({
+export const PageTitle = ({
   title,
   subTitle,
 }: {
   title: string;
-  subTitle: string;
+  subTitle?: string;
 }) => {
   return (
     <div className="flex flex-col w-full gap-3 my-[60px]">
       <span className="typo-title-01 text-white text-left whitespace-pre-line">
         {title}
       </span>
-      <span className="typo-body text-gray-20 text-left whitespace-pre-line">
-        {subTitle}
-      </span>
+      {subTitle && (
+        <span className="typo-body text-gray-20 text-left whitespace-pre-line">
+          {subTitle}
+        </span>
+      )}
     </div>
   );
 };
