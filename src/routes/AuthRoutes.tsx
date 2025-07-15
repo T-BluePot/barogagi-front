8d3d787ac3e614431b5b580f5213dfebc25434fd
Leeseoleem <leeseorim0029@gmail.com>
2025-07-15 19:08:32 +0900

    feat: SelectDatePage(지역 선택 페이지) 생성 및 라우트 연동

--- src/routes/AuthRoutes.tsx
+++ src/routes/AuthRoutes.tsx
@@ -1,10 +1,15 @@
 import { Routes, Route } from "react-router-dom";
 import AuthLandingPage from "@/pages/auth/AuthLandingPage";
 import TermsPage from "@/pages/auth/signup/TermsPage";
+/* 메인 페이지 */
+// 일정 생성 탭
+import SelectDatePage from "@/pages/main/plan/SelectDatePage";
 
 export const AuthRoutes = () => (
   <Routes>
     <Route path="/login" element={<AuthLandingPage />} />
     <Route path="/signup" element={<TermsPage />} />
+    {/* 일정 생성 */}
+    <Route path="/plan/date" element={<SelectDatePage />} />
   </Routes>
 );
