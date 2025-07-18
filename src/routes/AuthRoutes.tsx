b57059f51ec83398d4d7749637fb190b249fad36
정은우 <136785138+Jung-eunwoo@users.noreply.github.com>
2025-07-19 03:08:58 +0900

    feat: 랜딩 페이지 이메일 로그인 하기 버튼 클릭 시 로그인 페이지로 이동하도록 수정

--- src/routes/AuthRoutes.tsx
+++ src/routes/AuthRoutes.tsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 import AuthLandingPage from "@/pages/auth/AuthLandingPage";
 import TermsPage from "@/pages/auth/signup/TermsPage";
+import EmailLoginPage from "@/pages/auth/signin/EmailLoginPage";
 
 export const AuthRoutes = () => (
   <Routes>
     <Route path="/" element={<AuthLandingPage />} />
+    <Route path="/login" element={<EmailLoginPage />} />
     <Route path="/signup" element={<TermsPage />} />
   </Routes>
 );
