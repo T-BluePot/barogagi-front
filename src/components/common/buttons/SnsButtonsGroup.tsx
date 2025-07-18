51df0eaa8666b5d2be496d367bf50d93edf11b33
정은우 <136785138+Jung-eunwoo@users.noreply.github.com>
2025-07-19 02:36:33 +0900

    design: sns icon 로그인 버튼 생성

---
+++ src/components/common/buttons/SnsButtonsGroup.tsx
@@ -0,0 +1,33 @@
+import { SnsButton } from "@/components/common/buttons/SnsButton";
+interface SnsButtonsGroupProps {
+  onNaverClick: () => void;
+  onKakaoClick: () => void;
+  onGoogleClick: () => void;
+}
+
+export const SnsButtonsGroup = (props: SnsButtonsGroupProps) => {
+  return (
+    <div className="flex gap-6">
+      {/* 네이버 버튼 */}
+      <SnsButton
+        platform="naver"
+        onClick={props.onNaverClick}
+        className="text-white font-bold text-xl"
+      />
+
+      {/* 카카오 버튼 */}
+      <SnsButton
+        platform="kakao"
+        onClick={props.onKakaoClick}
+        className="text-black font-bold text-sm"
+      />
+
+      {/* 구글 버튼 */}
+      <SnsButton
+        platform="google"
+        onClick={props.onGoogleClick}
+        className="text-gray-700 font-bold text-xl"
+      />
+    </div>
+  );
+};
