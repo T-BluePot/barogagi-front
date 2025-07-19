34430874a623f07d84d8716b3d8f388d20240831
Leeseoleem <leeseorim0029@gmail.com>
2025-07-19 19:05:12 +0900

    chore: calender 전용 css 분리

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,9 @@ import { createRoot } from "react-dom/client";
 import "./globals.css";
 import App from "./App.tsx";
 
+import "react-datepicker/dist/react-datepicker.css";
+import "./components/main/plan/calendar.css";
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
