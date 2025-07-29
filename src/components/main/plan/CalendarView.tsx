a03ec9b4a230d2fa567b889ede9a26dc5e928eb4
Leeseoleem <leeseorim0029@gmail.com>
2025-07-29 19:00:08 +0900

    feat: CalendarView 컴포넌트 생성
    - 일정탭 메인 페이지 전용
    - 달력으로 보기 화면
    > 날짜 화면
    > 특정 날짜에 해당하는 일정 리스트 화면

---
+++ src/components/main/plan/CalendarView.tsx
@@ -0,0 +1,70 @@
+import { format } from "date-fns";
+
+import Calendar from "./Calendar";
+import type { CalendarProps } from "./Calendar";
+
+import { SimpleCourseCard } from "./SimpleCourseCard";
+import type { Schedule } from "@/types/schedule";
+
+import { formatDateToKorean } from "@/utils/date";
+
+interface CalendarViewProps extends CalendarProps {
+  schedules: Schedule[];
+  onEdit: () => void;
+  onDelete: () => void;
+}
+
+export const CalendarView = ({
+  schedules,
+  onEdit,
+  onDelete,
+  ...props
+}: CalendarViewProps) => {
+  // 선택한 날짜에 일정이 있는지
+  const selectedDate = props.selectedDate
+    ? format(props.selectedDate, "yyyy-MM-dd")
+    : "";
+
+  // 일정 여부 필터링
+  const filteredSchedules = schedules.filter(
+    (schedule) => schedule.date === selectedDate
+  );
+
+  return (
+    <div className="flex flex-col w-full h-full gap-4 pb-6 bg-gray-5">
+      <div className="flex-none">
+        <Calendar {...props} />
+      </div>
+      {props.selectedDate && (
+        <div className="flex flex-1 flex-col items-baseline px-6">
+          <div className="flex flex-col my-6 gap-3 items-baseline">
+            <span className="typo-title-02">
+              {formatDateToKorean(props.selectedDate)}
+            </span>
+            {filteredSchedules.length === 0 && (
+              <span className="typo-caption text-gray-80">
+                등록된 일정이 없습니다.
+              </span>
+            )}
+          </div>
+          <div className="flex flex-1 flex-col w-full gap-4">
+            {filteredSchedules.map((schedule, idx) => {
+              return (
+                <SimpleCourseCard
+                  key={idx}
+                  userNum={schedule.userNum}
+                  scheduleNum={schedule.scheduleNum}
+                  date={schedule.date}
+                  scheduleTitle={schedule.scheduleTitle}
+                  tags={schedule.tags}
+                  onEdit={onEdit}
+                  onDelete={onDelete}
+                />
+              );
+            })}
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
