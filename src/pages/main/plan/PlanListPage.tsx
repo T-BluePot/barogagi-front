c9f971217284c8e30349a8bbdc6720228efdcc58
Leeseoleem <leeseorim0029@gmail.com>
2025-07-29 19:25:55 +0900

    feat: PlanListPage 컴포넌트 생성
    - 일정탭 메인: 내 일정 화면
    > 목록으로 보기
    > 달력으로 보기
    - 두 버전의 화면 확인 가능

--- src/pages/main/plan/PlanListPage.tsx
+++ src/pages/main/plan/PlanListPage.tsx
@@ -1,7 +1,81 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+import { mockSchedules } from "@/mock/schedules";
+import { getMarkedDates } from "@/utils/getMarkedDates";
+
+import { TitleHeader } from "@/components/common/headers/TitleHeader";
+import { PlanViewToggleButton } from "@/components/main/plan/PlanViewToggleButton";
+import type { PlanViewType } from "@/components/main/plan/PlanViewToggleButton";
+import { CalendarView } from "@/components/main/plan/CalendarView";
+import { ListView } from "@/components/main/plan/ListView";
+import { AddScheduleButton } from "@/components/main/plan/AddScheduleButton";
+
 const PlanListPage = () => {
+  const navigate = useNavigate();
+
+  const [viewMode, setViewMode] = useState<PlanViewType>("list");
+
+  const toggleViewType = () => {
+    if (viewMode === "list") {
+      setViewMode("calendar");
+    } else setViewMode("list");
+  };
+
+  // list 모드용 데이터
+  const scheduleLists = mockSchedules.map((s) => ({
+    scheduleNum: s.scheduleNum,
+    userNum: s.membershipNo,
+    date: s.startDate,
+    scheduleTitle: s.scheduleNm,
+    tags: s.tags,
+  }));
+
+  // calendar 모드
+  const [selectedDate, setSelectedDate] = useState<Date | null>(null);
+  const markedDates = getMarkedDates(scheduleLists);
+
   return (
-    <div>
-      <span>일정탭 메인 페이지</span>
+    <div className="flex flex-col w-full min-h-screen gap-6 bg-gray-white">
+      <TitleHeader label="내 일정">
+        <PlanViewToggleButton
+          viewType={viewMode}
+          toggleViewType={toggleViewType}
+        />
+      </TitleHeader>
+      <div className="flex flex-1 w-full overflow-hidden">
+        {viewMode === "calendar" ? (
+          <div className="flex flex-1">
+            <CalendarView
+              selectedDate={selectedDate}
+              onChangeDate={(date) => setSelectedDate(date)}
+              markedDates={markedDates}
+              schedules={scheduleLists}
+              onDelete={() => {
+                // 삭제 로직
+              }}
+              onEdit={() => {
+                /* scheduleNum로 추천 루트 페이지 이동 **/
+              }}
+            />
+          </div>
+        ) : (
+          <div className="flex w-full px-6">
+            <ListView
+              schedules={scheduleLists}
+              onDelete={() => {
+                // 삭제 로직
+              }}
+              onEdit={() => {
+                /* scheduleNum로 추천 루트 페이지 이동 **/
+              }}
+            />
+          </div>
+        )}
+      </div>
+      <div className="fixed bottom-6 right-6">
+        <AddScheduleButton onAddSchedule={() => navigate("/plan/date")} />
+      </div>
     </div>
   );
 };
