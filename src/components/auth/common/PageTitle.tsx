a0076f5b6bbb046b44b2bff78da1e5a03dd720d9
Leeseoleem <leeseorim0029@gmail.com>
2025-08-27 20:10:50 +0900

    refactor: PageTitle 모드 추가 및 설정

--- src/components/auth/common/PageTitle.tsx
+++ src/components/auth/common/PageTitle.tsx
@@ -1,20 +1,36 @@
+import clsx from "clsx";
+
+type PAGEROUTE = "auth" | "main";
+
 export const PageTitle = ({
+  type = "auth",
   title,
   subTitle,
 }: {
+  type?: PAGEROUTE;
   title: string;
   subTitle?: string;
 }) => {
+  const containerClass = clsx(
+    "flex flex-col w-full",
+    type === "auth" ? "gap-3 my-[60px]" : "gap-2"
+  );
+
+  const titleClass = clsx(
+    "text-left whitespace-pre-line",
+    type === "auth"
+      ? "typo-title-01 text-white"
+      : "typo-title-02 text-gray-black"
+  );
+
+  const subTitleClass = clsx(
+    "text-left whitespace-pre-line",
+    type === "auth" ? "typo-body text-gray-20" : "typo-description text-gray-80"
+  );
   return (
-    <div className="flex flex-col w-full gap-3 my-[60px]">
-      <span className="typo-title-01 text-white text-left whitespace-pre-line">
-        {title}
-      </span>
-      {subTitle && (
-        <span className="typo-body text-gray-20 text-left whitespace-pre-line">
-          {subTitle}
-        </span>
-      )}
+    <div className={containerClass}>
+      <span className={titleClass}>{title}</span>
+      {subTitle && <span className={subTitleClass}>{subTitle}</span>}
     </div>
   );
 };
