eac5bfc9bd21348cb6023468e140d6aebc008932
Leeseoleem <leeseorim0029@gmail.com>
2025-09-05 18:45:09 +0900

    chore: SelectLocationPage 영역 텍스트 추가

--- src/constants/texts/main/plan/selectLocation.ts
+++ src/constants/texts/main/plan/selectLocation.ts
@@ -1,5 +1,9 @@
 export const SELECT_LOCATION_TEXT = {
   HEADER_TITLE: "지역 선택",
+  TITLE: "희망 지역을 선택해주세요",
+  SUB_TITLE: "최대 3개의 지역을 선택할 수 있어요",
+  PLACEHOLDER: "시/군/구로 검색해주세요",
+  ALERT_TAG: "태그는 최대 3개까지만 선택할 수 있습니다",
   NEXT_BUTTON: {
     enabled: "다음",
     disabled: "지역을 선택해주세요",
