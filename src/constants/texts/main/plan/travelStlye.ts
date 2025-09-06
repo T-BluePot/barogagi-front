9cc3b982cd636702261a8d48b4b73dd06e9ceacb
Leeseoleem <leeseorim0029@gmail.com>
2025-09-06 17:04:15 +0900

    chore: TravelStylePage 전용 텍스트 분리

---
+++ src/constants/texts/main/plan/travelStlye.ts
@@ -0,0 +1,9 @@
+export const TRAVEL_STYLE_TEXT = {
+  HEADER_TITLE: "여행 스타일 선택",
+  TITLE: "여행 취향 태그",
+  SUB_TITLE: "원하시는 여행 분위기를 태그로 알려주세요",
+  SEC_TITLE: "여행 참고사항",
+  SEC_SUB_TITLE: "여행을 더 즐겁게 만들 포인트를 남겨주세요",
+  PLACEHOLDER: "여행 스타일, 선호 사항 등을 자유롭게 적어주세요",
+  NEXT_BUTTON: "일정 생성하기",
+};
