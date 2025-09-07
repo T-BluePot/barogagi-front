c8f39bc336056c0e1dac2381397fb83a60830f8c
Leeseoleem <leeseorim0029@gmail.com>
2025-09-07 18:33:58 +0900

    chore: 텍스트 수정

--- src/constants/texts/main/plan/travelStlye.ts
+++ src/constants/texts/main/plan/travelStlye.ts
@@ -1,7 +1,7 @@
 export const TRAVEL_STYLE_TEXT = {
   HEADER_TITLE: "여행 스타일 선택",
-  TITLE: "여행 취향 태그",
-  SUB_TITLE: "원하시는 여행 분위기를 태그로 알려주세요",
+  TITLE: "여행 스타일 태그",
+  SUB_TITLE: "마음에 드는 여행 스타일 태그를 최대 5개까지 선택해주세요",
   SEC_TITLE: "여행 참고사항",
   SEC_SUB_TITLE: "여행을 더 즐겁게 만들 포인트를 남겨주세요",
   PLACEHOLDER: "여행 스타일, 선호 사항 등을 자유롭게 적어주세요",
