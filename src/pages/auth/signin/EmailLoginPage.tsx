42775a48bf6e03b17b9c85edaa1f7e42185712c1
Leeseoleem <leeseorim0029@gmail.com>
2025-09-23 01:29:38 +0900

    chore: 타이틀 컴포넌트 수정

--- src/pages/auth/signin/EmailLoginPage.tsx
+++ src/pages/auth/signin/EmailLoginPage.tsx
@@ -1,13 +1,14 @@
+import { PageTitle } from "@/components/auth/common/PageTitle";
 import { EmailLoginForm } from "@/components/auth/signin/EmailLoginForm";
 
 export default function EmailLoginPage() {
   return (
     <div className=" flex flex-col items-center justify-between p-4">
       <div className="flex flex-col items-start w-full mb-8">
-        <h1 className="typo-header text-gray-white text-left">이메일 로그인</h1>
-        <p className="text-gray-600 mb-6">
-          이메일과 비밀번호를 입력하여 로그인하세요.
-        </p>
+        <PageTitle
+          title="이메일로 로그인"
+          subTitle="이메일과 비밀번호를 입력하여 로그인하세요."
+        />
       </div>
       <EmailLoginForm />
     </div>
