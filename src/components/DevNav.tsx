d4c6eaba2a009d7527f9c7ff8f1196e63ae3f8fe
Leeseoleem <leeseorim0029@gmail.com>
2025-09-26 15:20:36 +0900

    chore: 테스트 용 라우트 수정

--- src/components/DevNav.tsx
+++ src/components/DevNav.tsx
@@ -7,7 +7,7 @@ const DevNav = () => {
       <button onClick={() => navigate("/signup")}>TermsPage</button>
       <button onClick={() => navigate("/")}>AuthLandingPage</button>
       <button onClick={() => navigate("/login")}>EmailLoginPage</button>
-      <button onClick={() => navigate("/plan/date")}>SelectDatePage</button>
+      <button onClick={() => navigate("/plan")}>SelectDatePage</button>
       {/* 필요한 라우트 버튼을 추가하세요 */}
     </nav>
   );
