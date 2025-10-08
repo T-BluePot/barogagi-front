4f54bff1d8cbf5dd3b841436eca06ef6bbffb35b
정은우 <136785138+Jung-eunwoo@users.noreply.github.com>
2025-10-08 19:26:10 +0900

    design: 홈페이지 컴포넌트 추가 및 상단 환영 문구 컴포넌트 추가

---
+++ src/pages/main/HomePage.tsx
@@ -0,0 +1,12 @@
+import HomeGreetingSection from "@/components/main/home/HomeGreetingSection";
+
+const HomePage = () => {
+  return (
+    <div className="flex flex-col h-full">
+      <HomeGreetingSection />
+      
+    </div>
+  );
+};
+
+export default HomePage;
