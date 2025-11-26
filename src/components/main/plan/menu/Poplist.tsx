90d2ffa99f15eceacd0ad429ab6998068f9011bf
Leeseoleem <leeseorim0029@gmail.com>
2025-11-27 01:48:42 +0900

    feat: Popper 메뉴 아이템 생성
    - 아이콘 + 라벨 형식

---
+++ src/components/main/plan/menu/Poplist.tsx
@@ -0,0 +1,32 @@
+import type React from "react";
+import clsx from "clsx";
+
+type Status = "normal" | "delete";
+
+export interface PopListProps {
+  status?: Status;
+  label: string;
+  children: React.ReactElement;
+  onClickItem: () => void;
+}
+
+export const PopList = ({
+  status = "normal",
+  label,
+  children,
+  onClickItem,
+}: PopListProps) => {
+  const textClass = clsx(
+    "typo-body",
+    status === "normal" ? "text-gray-black" : "text-alert-red"
+  );
+  return (
+    <button
+      onClick={onClickItem}
+      className="flex flex-row py-2 pl-3 pr-4 gap-3 justify-center"
+    >
+      <view>{children}</view>
+      <text className={textClass}>{label}</text>
+    </button>
+  );
+};
