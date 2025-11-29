66e55d874877779401813aa98d3410f4d3c4e160
Leeseoleem <leeseorim0029@gmail.com>
2025-11-29 17:53:47 +0900

    chore: InfoItemContainer에 disable 관련 로직 추가
    ### disabled
    - onClick 방지
    - 클릭 시 배경색 바뀌지 않음
    - 시간 리스트 아이템에서 사용

--- src/components/common/bottom-modal/items/InfoItemContainer.tsx
+++ src/components/common/bottom-modal/items/InfoItemContainer.tsx
@@ -1,23 +1,28 @@
-import type { IconType } from "@/types/main/plan/bottom-modal/itemsTypes";
-import { IconRenderer } from "./IconRenderer";
 import type React from "react";
+import type { IconType } from "@/types/main/plan/bottom-modal/itemsTypes";
+
+import { IconRenderer } from "./IconRenderer";
+import clsx from "clsx";
 
 interface InfoItemContainerProps {
   icon: IconType;
+  disabled?: boolean;
   onClick?: () => void;
   children: React.ReactNode;
 }
 
 export const InfoItemContainer = ({
   icon,
+  disabled = false,
   onClick,
   children,
 }: InfoItemContainerProps) => {
+  const containerClass = clsx(
+    "flex flex-row w-full min-h-14 py-4 px-6 items-center gap-2",
+    !disabled && "hover:bg-gray-5 active:bg-gray-5"
+  );
   return (
-    <button
-      className="flex flex-row w-full min-h-14 py-4 px-6 items-center gap-2 hover:bg-gray-5 active:bg-gray-5"
-      onClick={onClick}
-    >
+    <button className={containerClass} disabled={disabled} onClick={onClick}>
       <IconRenderer icon={icon} />
       {children}
     </button>
