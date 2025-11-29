74497018221ea0877eb5bc15add6d1943e5d14ff
Leeseoleem <leeseorim0029@gmail.com>
2025-11-29 12:29:34 +0900

    stlye: text tag 컴포넌트에 크기 속성 추가

--- src/components/common/tags/TextTag.tsx
+++ src/components/common/tags/TextTag.tsx
@@ -1,10 +1,22 @@
-type TextTagProps = {
+import clsx from "clsx";
+
+type Size = "default" | "large";
+
+interface TextTagProps {
+  size?: Size;
   label: string;
   hasHash?: boolean;
-};
+}
 
-export const TextTag = ({ label, hasHash = true }: TextTagProps) => {
-  const textClass = "text-gray-40 typo-tag";
+export const TextTag = ({
+  size = "default",
+  label,
+  hasHash = true,
+}: TextTagProps) => {
+  const textClass = clsx(
+    size === "default" && "text-gray-40 typo-tag",
+    size === "large" && "text-gray-80 typo-body"
+  );
 
   return (
     <div className="flex justify-center items-center gap-0.5">
