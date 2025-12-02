b5d3ce5c5c33195c1e8cce2333a8a9ae47d99c94
Leeseoleem <leeseorim0029@gmail.com>
2025-12-02 14:00:00 +0900

    chore: scheduleRoutesPage 전용 텍스트 파일 분리

---
+++ src/constants/texts/main/plan/routesCreate.ts
@@ -0,0 +1,24 @@
+export const ROUTES_CREATE_TEXT = {
+  CONFIRM_MODAL: {
+    CANCEL_LABEL: "취소",
+    CONFIRM_LABEL: "확인",
+    TITLE: "일정을 완성하시겠어요?",
+    CONTENTS: "완성한 일정은 ‘내 일정’ 탭에서 확인할 수 있어요.",
+  },
+  HEADER: {
+    DEFAULT_SCHEDULE_NAME: "오늘의 일정",
+    SCHEDULE_NAME_PLACEHOLDER: "일정에 어울리는 제목을 지어볼까요?",
+  },
+  POP_MENU: {
+    EDIT_LABLE: "수정하기",
+    DELETE_LABEL: "삭제하기",
+    DELETE_MODAL: {
+      TITLE: "일정을 식제하시겠어요?",
+      CONTENT:
+        "삭제된 일정은 다시 되돌릴 수 없어요.\n정말로 이 일정을 삭제할까요?",
+      CANCLE_LABEL: "취소",
+      CONFIRM_LABEL: "확인",
+    },
+  },
+  FOOTER_BUTTON_LABEL: "일정 완성하기",
+};
