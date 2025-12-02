c18ec6073ecc3a136e5d2a9b9877b965afec9e14
Leeseoleem <leeseorim0029@gmail.com>
2025-12-02 14:23:40 +0900

    feat: ScheduleRoutesPage 컴포넌트 1차 구현 완료
    - 일정 생성 완료 시 연동되는 페이지(추천 루트 페이지) 화면에 대한 UI 및 로직 구현 완료
    - 추후 다른 브런치에서 상세 페이지 관련 로직 추가 예정

---
+++ src/pages/main/plan/ScheduleRoutesPage.tsx
@@ -0,0 +1,156 @@
+import { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+
+import type { ScheduleRoutesPageProps } from "@/types/main/plan/scheduleRoutes";
+import type {
+  PlanDataProps,
+  PlanNoteMap,
+} from "@/types/main/plan/planListTypes";
+import { findPlanByNum } from "@/utils/main/plan/findPlan";
+
+import ScheduleRoutesContent from "@/components/main/plan/route/ScheduleRoutesContent";
+
+import { CreateScheduleModal } from "@/components/main/plan/create/CreateScheduleModal";
+import PlanFormModal from "@/components/main/plan/common/modal/PlanFormModal";
+import DeletePlanModal from "@/components/main/plan/create/DeletePlanModal";
+
+import { mockPlans } from "@/mock/plans"; // 추후 실제 데이터로 변경
+
+const ScheduleRoutesPage = ({ variant }: ScheduleRoutesPageProps) => {
+  const navigate = useNavigate();
+
+  // --- 넘어온 화면 확인용
+  const isCreate = variant === "create";
+  const isDetail = variant === "detail";
+
+  const { id } = useParams<{ id: string }>(); // /plan/:id/detail 에서 사용
+
+  useEffect(() => {
+    // 추후 서버 연동 시 수정 가능성 존재
+    setScheduleName("오늘의 일정");
+  }, []);
+
+  // ----- 헤더 영역 -----
+  const [scheduleName, setScheduleName] = useState<string>("");
+  const scheduleDate = new Date();
+
+  // ----- 일정 list 영역 -----
+  // 수정할 일정 함수
+  const [editPlanForm, setEditPlanForm] = useState<PlanDataProps | null>(null);
+
+  // ----- 일정 삭제하기 modal -----
+  const [isDeleteModalOpen, setIsDeleteModalOpen] = useState<boolean>(false);
+
+  const handleRequestDelete = (planNum: number) => {
+    // 추후 서버 연결 시 일정 번호 넘겨 삭제하는 로직 추가
+    setIsDeleteModalOpen(true);
+  };
+
+  // ----- 일정 수정하기 bottom modal -----
+  const [isEditModalOpen, setIsEditModalOpen] = useState<boolean>(false);
+
+  const filterPlanByNum = (plans: PlanDataProps[], planNum: number) => {
+    const currentMenuPlan = findPlanByNum(plans, planNum);
+    setEditPlanForm(currentMenuPlan);
+  };
+
+  const handleRequestEdit = (planNum: number) => {
+    filterPlanByNum(mockPlans, planNum);
+    setIsEditModalOpen(true);
+  };
+  // 컴포넌트 내부에서 사용할 메모 저장 변수
+  const [planNotes, setPlanNotes] = useState<PlanNoteMap>({});
+
+  const handleChangeNote = (planNum: number, nextValue: string) => {
+    setPlanNotes((prev) => ({
+      ...prev,
+      [planNum]: nextValue,
+    }));
+  };
+
+  // ----- 일정 confirm -----
+  const [isCreateModalOpen, setIsCreateModalOpen] = useState<boolean>(false);
+
+  const handleCloseCreateModal = () => setIsCreateModalOpen(false);
+
+  return (
+    <div className="flex flex-col w-full h-full bg-gray-5">
+      {/* 일정 생성 모달 */}
+      <CreateScheduleModal
+        isConfirmOpen={isCreateModalOpen}
+        onConfirmCancel={handleCloseCreateModal}
+        onConfirmConfirm={() => {
+          // 일정 생성 API 호출
+          handleCloseCreateModal();
+          navigate("/plan");
+        }}
+      />
+      {/* 팝메뉴- 일정 수정 및 삭제 모달 */}
+      {editPlanForm && (
+        <>
+          <PlanFormModal
+            action={{
+              isOpen: isEditModalOpen,
+              onClose: () => setIsEditModalOpen(false),
+              onConfirm: () => {},
+              onClickAction: () => {},
+            }}
+            info={{
+              mode: "Edit",
+              // --- 일정 기본 정보 ---
+              planNm: editPlanForm.plan.planNm,
+              startTime: editPlanForm.plan.startTime,
+              endTime: editPlanForm.plan.endTime,
+              address: editPlanForm.place.address,
+              onClickAdress: () => {},
+              // --- 일정 노트 아이템 관련 ---
+              note: planNotes[editPlanForm.plan.planNum],
+              noteValue: planNotes[editPlanForm.plan.planNum] ?? "",
+              onChangeNote: (next: string) =>
+                handleChangeNote(editPlanForm.plan.planNum, next),
+            }}
+          />
+          <DeletePlanModal
+            isOpen={isDeleteModalOpen}
+            onClickCancle={() => setIsDeleteModalOpen(false)}
+            onClickConfirm={() => {
+              // 일정 삭제 로직 추가
+              setIsDeleteModalOpen(false);
+            }}
+          />
+        </>
+      )}
+      {isCreate && (
+        <ScheduleRoutesContent
+          header={{
+            scheduleDate,
+            scheduleName,
+            onChangeScheduleName: setScheduleName,
+          }}
+          plans={mockPlans}
+          // isEditable 생략 → create 모드로 동작
+          isEditable={false}
+          footer={{
+            onClickConfirm: () => setIsCreateModalOpen(true),
+          }}
+        />
+      )}
+      {isDetail && (
+        <ScheduleRoutesContent
+          header={{
+            scheduleDate,
+            scheduleName,
+            onChangeScheduleName: setScheduleName,
+          }}
+          plans={mockPlans}
+          // isEditable 생략 → create 모드로 동작
+          isEditable={isDetail}
+          onRequestEdit={handleRequestEdit}
+          onRequestDelete={handleRequestDelete}
+        />
+      )}
+    </div>
+  );
+};
+
+export default ScheduleRoutesPage;
