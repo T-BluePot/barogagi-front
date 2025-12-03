24d08d1c2362189aba07fbdb6dde9273c9dc3e33
Leeseoleem <leeseorim0029@gmail.com>
2025-12-03 19:30:43 +0900

    chore: selectLocation 텍스트 추가

--- src/constants/texts/main/plan/selectLocation.ts
+++ src/constants/texts/main/plan/selectLocation.ts
@@ -5,7 +5,8 @@ export const SELECT_LOCATION_TEXT = {
   PLACEHOLDER: "시/군/구로 검색해주세요",
   ALERT_TAG: "태그는 최대 3개까지만 선택할 수 있습니다",
   NEXT_BUTTON: {
-    enabled: "다음",
-    disabled: "지역을 선택해주세요",
+    ADD_CURRENT_LOCATION_LABEL: "현재 위치 추가하기",
+    ENABLED: "다음",
+    DISABLED: "지역을 선택해주세요",
   },
 };
