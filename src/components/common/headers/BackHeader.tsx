c85e6d03669dde27325be36507b486bebf6a7158
Leeseoleem <leeseorim0029@gmail.com>
2025-12-04 02:28:13 +0900

    refactor: BackHeader 컴포넌트 children props 추가

--- src/components/common/headers/BackHeader.tsx
+++ src/components/common/headers/BackHeader.tsx
@@ -5,23 +5,28 @@ interface BackHeaderProps {
   label?: string;
   onClick: () => void;
   isDarkBg?: boolean;
+  children?: React.ReactNode;
 }
 
 export const BackHeader = ({
   isDarkBg = false,
   label,
   onClick,
+  children,
 }: BackHeaderProps) => {
   const colorClass = clsx(isDarkBg ? "text-gray-white" : "text-gray-black");
   return (
-    <header className="flex px-6 w-full h-[60px] items-center gap-6 select-none">
-      <ChevronLeftIcon
-        className={clsx("h-6 w-6 cursor-pointer", colorClass)}
-        onClick={onClick}
-      />
-      {label && (
-        <span className={clsx("typo-title-02", colorClass)}>{label}</span>
-      )}
+    <header className="flex px-6 w-full h-[60px] justify-between items-center gap-4 select-none">
+      <div className="flex flex-row gap-6 items-center">
+        <ChevronLeftIcon
+          className={clsx("h-6 w-6 cursor-pointer", colorClass)}
+          onClick={onClick}
+        />
+        {label && (
+          <span className={clsx("typo-title-02", colorClass)}>{label}</span>
+        )}
+      </div>
+      {children}
     </header>
   );
 };
