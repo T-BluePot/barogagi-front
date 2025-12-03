4e9e9efeabcad65d7fbbc24d4fd6f1ccceaaf427
Leeseoleem <leeseorim0029@gmail.com>
2025-12-03 18:51:15 +0900

    chore: 파일명 수정

--- src/utils/main/plan/findPlan.ts
+++
@@ -1,9 +0,0 @@
-import type { PlanDataProps } from "@/types/main/plan/planListTypes";
-
-export const findPlanByNum = (
-  plans: PlanDataProps[], // 일정 리스트
-  planNum: number | null // 선택된 일정 넘버
-): PlanDataProps | null => {
-  if (planNum == null) return null;
-  return plans.find((p) => p.plan.planNum === planNum) ?? null;
-};
