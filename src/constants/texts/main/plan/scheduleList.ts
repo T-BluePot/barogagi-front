06347bed5a3a36f7b012da88cecaa5b3418f9043
Leeseoleem <leeseorim0029@gmail.com>
2025-12-03 17:54:23 +0900

    chore: ScheduleListPage 전용 텍스트 분리 파일 생성

---
+++ src/constants/texts/main/plan/scheduleList.ts
@@ -0,0 +1,9 @@
+export const SCHEDULE_LIST_TEXT = {
+  HEADER: "내 일정",
+  DELETE_MODAL: {
+    CANCEL_LABEL: "취소",
+    CONFIRM_LABEL: "확인",
+    TITLE: "일정을 삭제하시겠어요?",
+    CONTENTS: "삭제된 일정은 복구할 수 없어요.\n정말 삭제하시겠어요?",
+  },
+};
