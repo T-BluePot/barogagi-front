eee4b0bdeca861a41fce2a8dfb96564f20c41ddd
Leeseoleem <leeseorim0029@gmail.com>
2025-12-04 02:49:53 +0900

    feat: RecentSearchHeader 컴포넌트 생성
    - 최근 검색어 + 삭제 버튼의 헤더 영역

---
+++ src/components/main/plan/search/RecentSearchHeader.tsx
@@ -0,0 +1,16 @@
+import { LOCATION_SEARCH_TEXT } from "@/constants/texts/main/plan/locationSearch";
+
+const RecentSearchHeader = ({ onclick }: { onclick: () => void }) => {
+  return (
+    <div className="flex flex-row h-12 px-6 justify-between items-center">
+      <p className="typo-caption">
+        {LOCATION_SEARCH_TEXT.HEADER.RECENT_SEARCH_TITLE}
+      </p>
+      <button onClick={onclick} className="typo-description text-gray-40">
+        {LOCATION_SEARCH_TEXT.HEADER.CLEAR_ALL_LABEL}
+      </button>
+    </div>
+  );
+};
+
+export default RecentSearchHeader;
