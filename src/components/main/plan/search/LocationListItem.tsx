f912e1f1b3dc01eaf0113075696702c4eb65489a
Leeseoleem <leeseorim0029@gmail.com>
2025-12-04 11:42:47 +0900

    feat: LocationListItem 컴포넌트 생성
    - TODO: 서버 연동 시 props 일부 수정

---
+++ src/components/main/plan/search/LocationListItem.tsx
@@ -0,0 +1,32 @@
+import { LocationIcon } from "./LocationIcon";
+
+export interface LocationListItemProps {
+  locationNm: string;
+  locationAdress: string;
+  onClick: () => void;
+}
+
+const LocationListItem = ({
+  locationNm,
+  locationAdress,
+  onClick,
+}: LocationListItemProps) => {
+  const subTextClass = `typo-description text-gray-60`;
+  return (
+    <div className="flex flex-row px-6 py-4 justify-between items-center border-b border-gray-5">
+      {/* 장소 정보 */}
+      <div className="flex flex-row items-center gap-5">
+        <LocationIcon />
+        <div className="flex flex-col items-baseline gap-1">
+          <p className="typo-subtitle"> {locationNm}</p>
+          <p className={subTextClass}> {locationAdress}</p>
+        </div>
+      </div>
+      <button onClick={onClick}>
+        <div className={subTextClass}>등록</div>
+      </button>
+    </div>
+  );
+};
+
+export default LocationListItem;
