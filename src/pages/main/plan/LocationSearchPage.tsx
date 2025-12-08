18c89d73713a281d3c76b892fb8b7ae56afe6469
Leeseoleem <leeseorim0029@gmail.com>
2025-12-08 22:48:43 +0900

    feat: LocationSearchPage 1차 구현 완료
    - 지역을 검색하고 겁색한 값을 usePlanEditStore에 넘겨주는 페이지
    - 부모 페이지에 Outlet 형식으로 종속되어 전역 저장소를 통해 변수 전달 후 뒤로가기 실행

--- src/pages/main/plan/LocationSearchPage.tsx
+++ src/pages/main/plan/LocationSearchPage.tsx
@@ -1,7 +1,57 @@
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+import { usePlanEditStore } from "@/stores/planEditStore";
+import type { EditPlanPlace } from "@/types/main/plan/bottom-modal/planFromTypes";
+import { LOCATION_SEARCH_TEXT } from "@/constants/texts/main/plan/locationSearch";
+import { useDebouncedKeyword } from "@/utils/useDebouncedKeyword";
+import { mockRecentLocations, mockLocations } from "@/mock/locations";
+
+import SearchBackHeader from "@/components/main/plan/common/SearchBackHeader";
+import RecentSearchSection from "@/components/main/plan/search/RecentSearchSection";
+import SearchLocationSection from "@/components/main/plan/search/SearchLocationSection";
+
 const LocationSearchPage = () => {
+  const navigate = useNavigate();
+
+  const [value, setValue] = useState<string>("");
+
+  const debouncedValue = useDebouncedKeyword({ value });
+  const hasValue = debouncedValue.length > 0;
+
+  const updatePlace = usePlanEditStore((state) => state.updatePlace);
+  const handleAddLocation = (place: EditPlanPlace) => {
+    updatePlace(place);
+    navigate(-1);
+  };
+
   return (
-    <div>
-      <p>하이</p>
+    <div className="fixed inset-0 z-[500]">
+      <div className="flex flex-col w-full h-full overflow-hidden bg-gray-white">
+        <div className="shrink-0 sticky top-0 z-10">
+          <SearchBackHeader
+            onClick={() => navigate(-1)}
+            searchProps={{
+              searchPlaceholder: LOCATION_SEARCH_TEXT.HEADER.PLACEHOLDER,
+              value: value,
+              setValue: setValue,
+              onClearSearchInput: () => setValue(""),
+            }}
+          />
+        </div>
+        {!hasValue ? (
+          <RecentSearchSection
+            recentLocations={mockRecentLocations}
+            onClickClear={() => {}}
+            onClickAddLocation={handleAddLocation}
+          />
+        ) : (
+          <SearchLocationSection
+            searchLocations={mockLocations}
+            onClickAddLocation={handleAddLocation}
+          />
+        )}
+      </div>
     </div>
   );
 };
