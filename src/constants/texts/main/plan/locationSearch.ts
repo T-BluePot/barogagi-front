72f345213d159d3c239c1f434942c8f225689b85
Leeseoleem <leeseorim0029@gmail.com>
2025-12-08 22:50:06 +0900

    feat: 검색 로직 전용 텍스트 상수 파일 LOCATION_SEARCH_TEXT 추가

---
+++ src/constants/texts/main/plan/locationSearch.ts
@@ -0,0 +1,18 @@
+export const LOCATION_SEARCH_TEXT = {
+  HEADER: {
+    PLACEHOLDER: "가고 싶은 장소를 검색해보세요!",
+    RECENT_SEARCH_TITLE: "최근 검색어",
+    CLEAR_ALL_LABEL: "전체 지우기",
+  },
+  CLEAR_ALL_MODAL: {
+    TITLE: "최근 검색어를 모두 삭제하시겠어요?",
+    CONTENT: "삭제한 검색어는 복구할 수 없습니다.",
+    CONFIRM_LABEL: "삭제",
+    CANCEL_LABEL: "취소",
+  },
+  ADD_LOCATION_MODAL: {
+    TITLE: "이 장소로 변경하시겠어요?",
+    CONFIRM_LABEL: "확인",
+    CANCEL_LABEL: "취소",
+  },
+};
