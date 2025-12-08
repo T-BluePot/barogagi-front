52fd9584c3c2ac5a34a676a372c2875e1c7fb146
Leeseoleem <leeseorim0029@gmail.com>
2025-12-08 17:02:57 +0900

    refactor: 버튼의 onClick에 AddLocationModal 연결

--- src/components/main/plan/search/LocationListItem.tsx
+++ src/components/main/plan/search/LocationListItem.tsx
@@ -1,20 +1,35 @@
+import { useState } from "react";
 import clsx from "clsx";
+
 import { LocationIcon } from "./LocationIcon";
+import AddLocationModal from "./AddLocationModal";
+
+interface modalProps {
+  handleConfirm: () => void;
+}
 
 export interface LocationListItemProps {
   locationNm: string;
   locationAdress: string;
-  onClick: () => void;
+  addModalProps: modalProps;
 }
 
 const LocationListItem = ({
   locationNm,
   locationAdress,
-  onClick,
+  addModalProps,
 }: LocationListItemProps) => {
   const subTextClass = `typo-description text-gray-60`;
+
+  const [isAddModalOpen, setIsAddModalOpen] = useState<boolean>(false);
   return (
     <div className="flex flex-row px-6 py-4 justify-between items-center border-b border-gray-5 active:bg-gray-5">
+      <AddLocationModal
+        isOpen={isAddModalOpen}
+        locationNm={locationNm}
+        handleConfirm={addModalProps?.handleConfirm}
+        handleCancel={() => setIsAddModalOpen(false)}
+      />
       {/* 장소 정보 */}
       <div className="flex flex-row items-center gap-5">
         <LocationIcon />
@@ -23,7 +38,7 @@ const LocationListItem = ({
           <p className={subTextClass}> {locationAdress}</p>
         </div>
       </div>
-      <button onClick={onClick}>
+      <button onClick={() => setIsAddModalOpen(true)}>
         <div className={clsx(subTextClass, "active:underline")}>등록</div>
       </button>
     </div>
