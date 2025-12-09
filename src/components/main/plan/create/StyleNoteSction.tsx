11968d1fef15ae30f18784f709531200b2aa6322
Leeseoleem <leeseorim0029@gmail.com>
2025-12-10 03:41:32 +0900

    feat: StyleNoteSction 컴포넌트 생성
    -  일정 참고사항 작성 영역 컴포넌트

---
+++ src/components/main/plan/create/StyleNoteSction.tsx
@@ -0,0 +1,28 @@
+import ScheduleStyleSectionLayout from "./ScheduleStyleSectionLayout";
+import TextInput from "@/components/common/inputs/TextInput";
+
+import { SCHEDULE_STYLE_TEXT } from "@/constants/texts/main/plan/scheduleStyle";
+
+const StyleNoteSction = ({
+  schedulelNotes,
+  setScheduleNotes,
+}: {
+  schedulelNotes: string;
+  setScheduleNotes: (value: string) => void;
+}) => {
+  return (
+    <ScheduleStyleSectionLayout
+      title={SCHEDULE_STYLE_TEXT.SEC_TITLE}
+      subTitle={SCHEDULE_STYLE_TEXT.SEC_SUB_TITLE}
+    >
+      <TextInput
+        size="large"
+        placeholder={SCHEDULE_STYLE_TEXT.PLACEHOLDER}
+        value={schedulelNotes}
+        onChange={setScheduleNotes}
+      />
+    </ScheduleStyleSectionLayout>
+  );
+};
+
+export default StyleNoteSction;
