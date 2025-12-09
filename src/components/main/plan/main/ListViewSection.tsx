5c34a4a9717746ae24dfe0dbbc13f7cd5b66035b
Leeseoleem <leeseorim0029@gmail.com>
2025-12-09 18:32:33 +0900

    feat: ListViewSection 컴포넌트 생성
    - 일정 카드를 리스트 형식으로 보여주는 영역 컴포넌트

---
+++ src/components/main/plan/main/ListViewSection.tsx
@@ -0,0 +1,40 @@
+import { ListView, type ScheduleCardActions } from "./ListView";
+import type { Schedule } from "@/types/scheduleTypes";
+import ListViewSectionDivider from "./ListViewSectionDivider";
+
+interface ListViewSectionProps extends ScheduleCardActions {
+  schedules: Schedule[]; // 현재 일정 (필수)
+  pastSchedules?: Schedule[]; // 지난 일정 (선택)
+}
+
+const ListViewSection = ({
+  schedules, // 필수
+  pastSchedules, // 선택
+  onClickCard,
+  onDelete,
+}: ListViewSectionProps) => {
+  const hasPast = pastSchedules && pastSchedules.length > 0;
+  return (
+    <div className="flex flex-col w-full h-full pb-6 gap-4 overflow-y-scroll hide-scrollbar">
+      <ListView
+        schedules={schedules}
+        onClickCard={onClickCard}
+        onDelete={onDelete}
+      />
+      {/* 지난 일정이 있을 때: 구분선 + 지난 일정 리스트 */}
+      {hasPast && (
+        <div className="flex flex-col gap-4">
+          <ListViewSectionDivider />
+          <ListView
+            isPast
+            schedules={pastSchedules!}
+            onClickCard={onClickCard}
+            onDelete={onDelete}
+          />
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default ListViewSection;
