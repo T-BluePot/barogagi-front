acb9020b92d1361938f829498c6ef96fa9a771d7
Leeseoleem <leeseorim0029@gmail.com>
2025-12-09 21:52:51 +0900

    refactor: CalendarTitle 컴포넌트 내  subTitle 타입 및 영역 확장

--- src/components/main/plan/CalendarTitle.tsx
+++ src/components/main/plan/CalendarTitle.tsx
@@ -1,11 +1,20 @@
 import { formatDateToKorean } from "@/utils/date";
 
-export const CalendarTitle = ({ selectedDate }: { selectedDate: Date }) => {
+export const CalendarTitle = ({
+  selectedDate,
+  subTitle,
+}: {
+  selectedDate: Date;
+  subTitle?: string;
+}) => {
   return (
-    <div className="flex px-6">
+    <div className="flex flex-col items-baseline px-6 gap-3">
       <span className="typo-title-01 text-gray-black">
         {formatDateToKorean(selectedDate)}
       </span>
+      {subTitle && subTitle.length !== 0 && (
+        <span className="typo-subtitle text-gray-80">{subTitle}</span>
+      )}
     </div>
   );
 };
