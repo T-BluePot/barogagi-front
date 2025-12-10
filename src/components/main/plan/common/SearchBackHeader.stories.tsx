666e96ad56d950037c60cce533c78b25bddacb35
Leeseoleem <leeseorim0029@gmail.com>
2025-12-10 18:02:38 +0900

    feat: SearchBackHeader 컴포넌트 스토리북 연동

---
+++ src/components/main/plan/common/SearchBackHeader.stories.tsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+import type { Meta, StoryObj } from "@storybook/react-vite";
+import SearchBackHeader from "./SearchBackHeader";
+
+const meta = {
+  title: "Components/Main/Plan/Common/SearchBackHeader",
+  component: SearchBackHeader,
+  tags: ["autodocs"],
+  args: {
+    onClick: () => console.log("Back button clicked"),
+    searchProps: {
+      searchPlaceholder: "가고 싶은 장소를 검색해보세요!",
+      value: "",
+      setValue: (next: string) => console.log("Set value:", next),
+      onClearSearchInput: () => console.log("Clear search input"),
+    },
+  },
+} satisfies Meta<typeof SearchBackHeader>;
+
+export default meta;
+type Story = StoryObj<typeof SearchBackHeader>;
+
+export const Default: Story = {
+  render: (args) => {
+    const [value, setValue] = useState<string>("");
+
+    const handleClearValue = () => setValue("");
+    return (
+      <SearchBackHeader
+        {...args}
+        searchProps={{
+          ...args.searchProps,
+          value: value,
+          setValue: setValue,
+          onClearSearchInput: handleClearValue,
+        }}
+      />
+    );
+  },
+};
