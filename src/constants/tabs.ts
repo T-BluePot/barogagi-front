f34b6d44c3bc35899abdc410d5f656e0b0d73419
Leeseoleem <leeseorim0029@gmail.com>
2025-12-17 00:38:14 +0900

    chore: 탭 아이템 순서 변경

--- src/constants/tabs.ts
+++ src/constants/tabs.ts
@@ -1,8 +1,8 @@
 import { ROUTES } from "./routes";
 
 export const TAB_CONFIG = {
-  home: { path: ROUTES.TABS.MAIN, ariaLabel: "홈" },
   plan: { path: ROUTES.TABS.PLAN, ariaLabel: "일정" },
+  home: { path: ROUTES.TABS.MAIN, ariaLabel: "홈" },
   my: { path: ROUTES.TABS.USER, ariaLabel: "내 정보" },
 } as const;
 
