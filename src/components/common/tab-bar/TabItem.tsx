cb7c51d15018f5eda53765230428f3d776341a2c
Leeseoleem <leeseorim0029@gmail.com>
2025-12-16 17:01:14 +0900

    feat: 탭 바 아이템 컴포넌트 생성

---
+++ src/components/common/tab-bar/TabItem.tsx
@@ -0,0 +1,34 @@
+import type { TabVariant } from "@/types/main/tabBarTypes";
+
+import AddOutlinedIcon from "@mui/icons-material/AddOutlined";
+import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
+import PersonOutlineOutlinedIcon from "@mui/icons-material/PersonOutlineOutlined";
+
+interface TabItemProps {
+  variant: TabVariant; // 어떤 탭인지
+  isActive: boolean; // 현재 활성 상태
+  onClick?: () => void; // 탭 클릭 액션
+}
+
+export const TabItem = ({
+  variant = "home",
+  isActive = false,
+  onClick,
+}: TabItemProps) => {
+  const iconClass = {
+    fontSize: 40,
+    color: isActive ? "#bafd4f" : "#ffffff",
+  };
+
+  return (
+    <button
+      className="flex w-10 h-10 justify-center items-center"
+      type="button"
+      onClick={onClick}
+    >
+      {variant === "home" && <HomeOutlinedIcon sx={iconClass} />}
+      {variant === "plan" && <AddOutlinedIcon sx={iconClass} />}
+      {variant === "my" && <PersonOutlineOutlinedIcon sx={iconClass} />}
+    </button>
+  );
+};
