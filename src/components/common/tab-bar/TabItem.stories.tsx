724ac34ad81e15d8abfcefd77139840f53f1008a
Leeseoleem <leeseorim0029@gmail.com>
2025-12-17 17:44:15 +0900

    rename: TabItem 스토리 이름 변경

--- src/components/common/tab-bar/TabItem.stories.tsx
+++ src/components/common/tab-bar/TabItem.stories.tsx
@@ -23,19 +23,19 @@ export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-export const HomeActive: Story = {
+export const Home: Story = {
   args: {
     variant: "home",
   },
 };
 
-export const PlanActive: Story = {
+export const Plan: Story = {
   args: {
     variant: "plan",
   },
 };
 
-export const MyActive: Story = {
+export const My: Story = {
   args: {
     variant: "my",
   },
