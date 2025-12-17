e65b555121cf2ea17c09dac4f42e798c6efdace8
Leeseoleem <leeseorim0029@gmail.com>
2025-12-17 17:43:19 +0900

    design: rounded css 조정

--- src/components/common/tab-bar/BottomTabBar.tsx
+++ src/components/common/tab-bar/BottomTabBar.tsx
@@ -7,7 +7,7 @@ const BottomTabBar = () => {
   const tabs = Object.keys(TAB_CONFIG) as TabVariant[];
   return (
     <nav aria-label="Tabs" className="fixed bottom-0 left-0 right-0 z-30">
-      <ul className="flex w-full h-[60px] justify-around items-center rounded-t-4xl bg-gray-black shadow-[0_-8px_20px_rgba(0,0,0,0.12)]">
+      <ul className="flex w-full h-[60px] justify-around items-center rounded-t-[40px] bg-gray-black shadow-[0_-8px_20px_rgba(0,0,0,0.12)]">
         {tabs.map((tab) => {
           const { path, ariaLabel } = TAB_CONFIG[tab];
           return (
