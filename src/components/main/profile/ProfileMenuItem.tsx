ccce9cf6f44c5cf9ed90c3ce716c036e411ab5da
정은우 <136785138+Jung-eunwoo@users.noreply.github.com>
2026-01-24 13:47:42 +0900

    refactor: 프로젝트에 정의된 icon을 사용하도록 수정

--- src/components/main/profile/ProfileMenuItem.tsx
+++ src/components/main/profile/ProfileMenuItem.tsx
@@ -1,5 +1,5 @@
-import { ChevronRightIcon } from "@heroicons/react/24/solid";
 import type { ProfileMenuItemProps } from "@/types/profileTypes";
+import IconBox from "@/components/common/IconBox";
 
 const ProfileMenuItem = ({ label, onClick }: ProfileMenuItemProps) => {
   return (
@@ -9,7 +9,12 @@ const ProfileMenuItem = ({ label, onClick }: ProfileMenuItemProps) => {
       onClick={onClick}
     >
       <span className="typo-body text-gray-black">{label}</span>
-      <ChevronRightIcon className="w-5 h-5 text-gray-400" />
+      <IconBox
+        name="arrow_forward_ios"
+        width={16}
+        weight={100}
+        className="w-5 h-5 text-gray-400"
+      />
     </button>
   );
 };
