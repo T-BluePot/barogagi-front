6b511647bbd9030b273fdd012d11cbe07ca19c3f
정은우 <136785138+Jung-eunwoo@users.noreply.github.com>
2026-01-24 13:41:18 +0900

    design: implement profile page and related components

---
+++ src/pages/main/profile/ProfileEditPage.tsx
@@ -0,0 +1,11 @@
+const ProfileEditPage = () => {
+  return (
+    <div className="flex flex-col w-full h-full bg-gray-black text-white px-6">
+      <div className="flex flex-col items-center justify-center h-full">
+        Profile Edit Page
+      </div>
+    </div>
+  );
+};
+
+export default ProfileEditPage;
