5235ddcc8e8f0d274d4c1df640db8033077e7caf
Lee seo Leem <138491614+Leeseoleem@users.noreply.github.com>
2026-02-05 16:36:50 +0900

    refactor: 인증 전송 실패 시 예외 처리 및 피드백 로직 추가

--- src/pages/auth/verify/VerifyPage.tsx
+++ src/pages/auth/verify/VerifyPage.tsx
@@ -1,11 +1,13 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate, useParams, useLocation } from "react-router-dom";
+import { AxiosError } from "axios";
 
 import { VERIFY_TEXT } from "@/constants/texts/auth/verify";
+import { ROUTES } from "@/constants/routes";
 
 import { PageTitle } from "@/components/auth/common/PageTitle";
 import { VerifyForm } from "@/components/auth/verify/VerifyForm";
-import { ROUTES } from "@/constants/routes";
+import VerifyErrorModal from "@/components/auth/verify/VerifyErrorModal";
 
 import { sendVerification } from "@/api/queries";
 import { VERIFICATION_REQUEST_TYPE } from "@/constants/verificationTypes";
@@ -68,21 +70,44 @@ const VerifyPage = () => {
     ? FLOW_CONFIG[flow as Flow]
     : FLOW_CONFIG["signup-verify"];
 
+  const [errorText, setErrorText] = useState("");
+  const [isErrorModalOpen, setIsErrorModalOpen] = useState(false);
+
   const handleNext = async (phone: string) => {
     const tel = phone.trim();
     if (!tel) return;
 
-    if (flow === "signup-verify") {
-      await sendVerification(tel, VERIFICATION_REQUEST_TYPE.JOIN_MEMBERSHIP);
-    } else {
-      await sendVerification(tel); // signup 로직이 아닌 경우 type 미전달
-    }
+    try {
+      if (flow === "signup-verify") {
+        await sendVerification(tel, VERIFICATION_REQUEST_TYPE.JOIN_MEMBERSHIP);
+      } else {
+        await sendVerification(tel); // signup 로직이 아닌 경우 type 미전달
+      }
 
-    navigate(current.nextPath, { state: { phone: tel } });
+      navigate(current.nextPath, { state: { phone: tel } });
+    } catch (error) {
+      if (error instanceof AxiosError) {
+        setErrorText(
+          error.response?.data?.message ??
+            "인증 번호 전송을 실패했습니다. \n잠시 후 다시 시도해주새요."
+        );
+      } else {
+        setErrorText(
+          "인증 번호 전송을 실패했습니다. \n잠시 후 다시 시도해주새요."
+        );
+      }
+
+      setIsErrorModalOpen(true); // 모달 열기
+    }
   };
 
   return (
     <div className="flex flex-col w-full px-6">
+      <VerifyErrorModal
+        isOpen={isErrorModalOpen}
+        message={errorText}
+        onClick={() => setIsErrorModalOpen(false)}
+      />
       <PageTitle title={current.title} subTitle={current.subTitle} />
       <VerifyForm
         label={current.label}
