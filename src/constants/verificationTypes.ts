63a0b69aa650dc4fa0e27d27c7a063d09af4dadf
Lee seo Leem <138491614+Leeseoleem@users.noreply.github.com>
2026-03-02 02:41:34 +0900

    chore: 전화번호 중복 응답 코드 상수 정의

--- src/constants/verificationTypes.ts
+++ src/constants/verificationTypes.ts
@@ -8,3 +8,11 @@ export const VERIFICATION_REQUEST_TYPE = {
 /** VERIFICATION_REQUEST_TYPE 값 유니온 타입 */
 export type VerificationType =
   (typeof VERIFICATION_REQUEST_TYPE)[keyof typeof VERIFICATION_REQUEST_TYPE];
+
+export const API_CODE = {
+  TEL_AVAILABLE: "T200", // 중복 없음
+  TEL_DUPLICATED: "S400", // 중복 있음
+  INVALID_ACCESS: "A100",
+  NEED_INPUT: "C101",
+  SERVER_ERROR: "COMMON-500",
+} as const;
