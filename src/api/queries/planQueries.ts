49119127619c4e7fd25cfd23ec03965d2f40bd15
Lee seo Leem <138491614+Leeseoleem@users.noreply.github.com>
2026-03-09 03:07:56 +0900

    chore: 일정 생성 쿼리문 apiKeyHttp 로 수정

--- src/api/queries/planQueries.ts
+++ src/api/queries/planQueries.ts
@@ -2,7 +2,7 @@
  * 일정(Schedule) 관련 API 요청 함수
  */
 
-import { http } from "../client";
+import { apiKeyHttp, http } from "../client";
 import { ENDPOINTS } from "../endpoints";
 import type {
   BaseResponse,
@@ -31,7 +31,7 @@ export const getScheduleDetail = async (scheduleNum: number) => {
 
 /** 일정 생성 (AI 생성 등 초기 생성) */
 export const createSchedule = async (data: ScheduleRegistReqDTO) => {
-  const response = await http.post<BaseResponse<ScheduleRegistResDTO>>(
+  const response = await apiKeyHttp.post<BaseResponse<ScheduleRegistResDTO>>(
     ENDPOINTS.SCHEDULE.CREATE,
     data
   );
