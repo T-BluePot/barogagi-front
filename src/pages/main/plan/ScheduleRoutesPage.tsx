89eff1b0ae6a3c5c7538b12e8eeab8fe6fae079e
Lee seo Leem <138491614+Leeseoleem@users.noreply.github.com>
2026-03-09 13:57:25 +0900

    chore: 사용 안 하는 import 구문 제거

--- src/pages/main/plan/ScheduleRoutesPage.tsx
+++ src/pages/main/plan/ScheduleRoutesPage.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate, useParams, Outlet } from "react-router-dom";
 import { AxiosError } from "axios";
 import toast from "react-hot-toast";
