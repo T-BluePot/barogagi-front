307e4f423c5b056fbae8828c4b101e7a5152dedf
Lee seo Leem <138491614+Leeseoleem@users.noreply.github.com>
2026-05-15 18:05:03 +0900

    design: 탭바 없는 화면 하단 safe-area 적용 (폰 하단바 가림 수정)

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -122,7 +122,7 @@ export const Layout = ({ children }: LayoutProps) => {
 
   return (
     <div
-      className={`h-screen flex flex-col pt-safe ${
+      className={`h-screen flex flex-col pt-safe pb-safe ${
         isHeaderDark ? "bg-gray-black" : "bg-white"
       }`}
     >
