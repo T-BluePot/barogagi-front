93dfd61806ca67be636beb14d9ff7f764466d906
정은우 <136785138+Jung-eunwoo@users.noreply.github.com>
2026-05-31 14:25:27 +0900

    chore: FCM appVersion용 VITE_APP_VERSION env 타입 추가

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -9,6 +9,11 @@ interface ImportMetaEnv {
    * 실기기(브릿지 환경)에선 무시되고 네이티브가 발급한 토큰이 우선한다.
    */
   readonly VITE_FCM_TEST_TOKEN?: string;
+  /**
+   * 앱 빌드 버전. FCM 토큰 등록 시 appVersion 필드로 서버에 전송한다.
+   * 빌드 시점에 CI/배포 환경에서 주입하는 것을 권장한다.
+   */
+  readonly VITE_APP_VERSION?: string;
 }
 
 interface ImportMeta {
